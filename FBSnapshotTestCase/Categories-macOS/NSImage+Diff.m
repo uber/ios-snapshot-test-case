290aaca406df03965850d5630781fa353b2b3cd8
i.misuno <ivan.misuno@albelli.com>
2018-03-12 12:37:50 +0100

    MacOS port: adjust `-fb_compareWithImage:tolerance:` and `-fb_diffWithImage:` implementations.
    Summary:
    Port `-fb_compareWithImage:tolerance:` and `-fb_diffWithImage:` to mac.
    Existing tests pass.
    TODO: port `NSImage+Snapshot` category.

--- FBSnapshotTestCase/Categories-macOS/NSImage+Diff.m
+++ FBSnapshotTestCase/Categories-macOS/NSImage+Diff.m
@@ -34,24 +34,37 @@
 
 - (NSImage *)fb_diffWithImage:(NSImage *)image
 {
-  return nil;
-//  if (!image) {
-//    return nil;
-//  }
-//  CGSize imageSize = CGSizeMake(MAX(self.size.width, image.size.width), MAX(self.size.height, image.size.height));
-//  UIGraphicsBeginImageContextWithOptions(imageSize, YES, 0);
-//  CGContextRef context = UIGraphicsGetCurrentContext();
-//  [self drawInRect:CGRectMake(0, 0, self.size.width, self.size.height)];
-//  CGContextSetAlpha(context, 0.5);
-//  CGContextBeginTransparencyLayer(context, NULL);
-//  [image drawInRect:CGRectMake(0, 0, image.size.width, image.size.height)];
-//  CGContextSetBlendMode(context, kCGBlendModeDifference);
-//  CGContextSetFillColorWithColor(context,[UIColor whiteColor].CGColor);
-//  CGContextFillRect(context, CGRectMake(0, 0, self.size.width, self.size.height));
-//  CGContextEndTransparencyLayer(context);
-//  UIImage *returnImage = UIGraphicsGetImageFromCurrentImageContext();
-//  UIGraphicsEndImageContext();
-//  return returnImage;
+  if (!image) {
+    return nil;
+  }
+  CGSize imageSize = CGSizeMake(MAX(self.size.width, image.size.width), MAX(self.size.height, image.size.height));
+  NSImage *resultImage = [[NSImage alloc] initWithSize:imageSize];
+  NSBitmapImageRep* rep = [[NSBitmapImageRep alloc]
+                           initWithBitmapDataPlanes:NULL
+                           pixelsWide:imageSize.width
+                           pixelsHigh:imageSize.height
+                           bitsPerSample:8
+                           samplesPerPixel:4
+                           hasAlpha:YES
+                           isPlanar:NO
+                           colorSpaceName:NSCalibratedRGBColorSpace
+                           bytesPerRow:0
+                           bitsPerPixel:0];
+  [resultImage addRepresentation:rep];
+  [resultImage lockFocus];
+
+  CGContextRef context = [[NSGraphicsContext currentContext] graphicsPort];
+  [self drawInRect:CGRectMake(0, 0, self.size.width, self.size.height)];
+  CGContextSetAlpha(context, 0.5);
+  CGContextBeginTransparencyLayer(context, NULL);
+  [image drawInRect:CGRectMake(0, 0, image.size.width, image.size.height)];
+  CGContextSetBlendMode(context, kCGBlendModeDifference);
+  CGContextSetFillColorWithColor(context,[NSColor whiteColor].CGColor);
+  CGContextFillRect(context, CGRectMake(0, 0, self.size.width, self.size.height));
+  CGContextEndTransparencyLayer(context);
+  [resultImage unlockFocus];
+
+  return resultImage;
 }
 
 @end
