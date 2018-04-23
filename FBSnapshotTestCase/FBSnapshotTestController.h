e8418651f1cfae782163881e86c9457eef7c9525
Alan Zeino <alanz@uber.com>
2018-04-23 15:03:43 -0700

    Update header file copyright

--- FBSnapshotTestCase/FBSnapshotTestController.h
+++ FBSnapshotTestCase/FBSnapshotTestController.h
@@ -1,5 +1,6 @@
 /*
- *  Copyright (c) 2015-present, Facebook, Inc.
+ *  Copyright (c) 2017-2018, Uber Technologies, Inc.
+ *  Copyright (c) 2015-2018, Facebook, Inc.
  *
  *  This source code is licensed under the MIT license found in the
  *  LICENSE file in the root directory of this source tree.
