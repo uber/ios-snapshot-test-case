d8f70434c95ffc10f90c6f749c2f02d04f9392a1
bfernandesbfs <bruno.fernandesbfs@gmail.com>
2019-10-23 18:17:26 -0300

    Add references images for test SPM

--- Sample/iOSSnapshotTestCaseSPMDemo/iOSSnapshotTestCaseSPMDemoTests/iOSSnapshotTestCaseSPMDemoTests.swift
+++ Sample/iOSSnapshotTestCaseSPMDemo/iOSSnapshotTestCaseSPMDemoTests/iOSSnapshotTestCaseSPMDemoTests.swift
@@ -13,7 +13,7 @@ class iOSSnapshotTestCaseSPMDemoTests: FBSnapshotTestCase {
 
     override func setUp() {
         super.setUp()
-        recordMode = true
+        recordMode = false
     }
 
     func testExample() {
