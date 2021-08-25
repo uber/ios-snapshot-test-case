9e60dee402affde32241f312f857eeaa960423a2
Alan Zeino <alanz@uber.com>
2021-08-24 17:04:44 -0700

    Fix Carthage Demo

--- build.sh
+++ build.sh
@@ -49,7 +49,7 @@ function ci_demo_preprocessor() {
 function ci_carthage_demo() {
     NAME=$1
     pushd iOSSnapshotTestCaseCarthageDemo
-    carthage bootstrap
+    carthage bootstrap --no-use-binaries --use-xcframeworks
     xcodebuild -project iOSSnapshotTestCaseCarthageDemo.xcodeproj \
                -scheme iOSSnapshotTestCaseCarthageDemo \
                -destination "platform=iOS Simulator,name=${NAME}" \
