cdcc374f86aa8bd79d8e308b021cce4f5704b512
Alan Zeino <alanz@uber.com>
2021-10-21 17:52:21 -0700

    Use bundle instead of file path hackery

--- src/iOSSnapshotTestCaseTests/FBSnapshotControllerTests.m
+++ src/iOSSnapshotTestCaseTests/FBSnapshotControllerTests.m
@@ -200,17 +200,10 @@
 
 - (UIImage *)_bundledImageNamed:(NSString *)name type:(NSString *)type
 {
-    #if BAZEL
     NSBundle *bundle = [NSBundle bundleForClass:[FBSnapshotControllerTests class]];
     NSString *path = [bundle pathForResource:name ofType:type];
     NSData *data = [[NSData alloc] initWithContentsOfFile:path];
     return [[UIImage alloc] initWithData:data];
-    #else
-    NSURL *url = [[NSURL fileURLWithPath: @__FILE__] URLByDeletingLastPathComponent];
-    NSString *path = [[url URLByAppendingPathComponent: [NSString stringWithFormat:@"%@.%@", name, type]] path];
-    NSData *data = [[NSData alloc] initWithContentsOfFile:path];
-    return [[UIImage alloc] initWithData:data];
-    #endif
 }
 
 @end
